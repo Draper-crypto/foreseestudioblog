f65a82a1480d1b028bfd307780ab01e60c2b5954
Draper-crypto <62155410+Draper-crypto@users.noreply.github.com>
2023-12-27 22:46:53 +0800

    备份的部分源文件
    备份的部分源文件

---
+++ 论坛/恢复包.sh
@@ -0,0 +1,153 @@
+# 主题
+composer require dalez/fluent-flarum
+# 中文安装包
+composer require flarum-lang/chinese-simplified:dev-master
+# id slug 驱动程序
+composer require pipecraft/flarum-ext-id-slug
+# 下拉菜单
+composer require fof/links
+# 置顶和排序（未知好坏）
+composer require michaelbelgium/flarum-discussion-views
+# 更新报错
+composer require symfony/mailer
+# Flarum 论坛的基于权限的公共用户目录扩展。
+composer require fof/user-directory:"*"
+# FoF用户简介，将用户简介添加到用户配置文件
+composer require fof/user-bio:"*"
+# 要求用户在使用论坛之前接受使用条款
+composer require fof/terms
+# FoF 网站地图，生成站点地图
+composer require fof/sitemap
+# FoF 民意调查，一个 Flarum 扩展，可将投票添加到您的讨论中
+composer require fof/polls
+# FoF夜间模式，添加夜间模式选项供用户在 Flarum 论坛上使用
+composer require fof/nightmode:"*"
+# FoF语言学家，自定义翻译
+composer require fof/linguist
+# FoF门卫，为用户创建邀请码以注册并分配到组
+composer require fof/doorman:"*"
+# FoF 最佳答案，在讨论中将帖子标记为最佳答案
+composer require fof/best-answer:"*"
+# FoF社交资料，将自定义社交媒体页面添加到您的用户个人资料中
+composer require fof/socialprofile
+# FoF社交，添加一个按钮以通过社交网络共享讨论
+composer require fof/share-social
+# FoF个人资料图片裁剪，上传个人资料图片时允许裁剪
+composer require fof/profile-image-crop:"*"
+# FoF HTML 错误，添加自定义 HTML 错误页面
+composer require fof/html-errors
+# FoF 首页，将讨论推到Flarum论坛索引的前面！
+composer require fof/frontpage
+# FoF草稿，允许用户创建帖子和讨论草稿
+composer require fof/drafts:"*"
+# FoF Cookie 同意，可定制的 cookie 同意通知
+composer require fof/cookie-consent
+# 渐进式 Web 应用程序，将您的 flarum 网站变成一个渐进式 Web 应用程序！
+composer require askvortsov/flarum-pwa
+# Markdown 表格
+composer require askvortsov/flarum-markdown-tables:*
+# 首次批准,保留新用户的帖子和讨论以供批准
+composer require clarkwinkelmann/flarum-ext-first-post-approval
+# FFans 剪贴板js,添加一种现代方法，将代码复制到剪贴板，并显示代码语言。
+# https://extiverse.com/extension/ffans/clipboardjs
+composer require ffans/clipboardjs
+# FoF过滤，从论坛帖子中过滤掉单词和短语
+composer require fof/filter:"*"
+# 清单,Flarum 的清单
+composer require askvortsov/flarum-checklist:*
+# 级别排名,将关卡/经验栏添加到您的 flarum 论坛。
+composer require ianm/level-ranks:"*"
+# 将讨论粘贴、超级粘贴或标记将讨论粘贴到列表顶部。
+# https://extiverse.com/extension/the-turk/flarum-stickiest
+composer require the-turk/flarum-stickiest
+# 火烈鸟，Flarum 的简单表情符号管理器
+composer require the-turk/flarum-flamoji
+# Fancybox 是一种支持触摸的响应式工具，用于在浮动在网页顶部的“灯箱”中显示图像。
+composer require darkle/fancybox
+# UI 选项卡，向 Flarum 添加一个 UI 选项卡。
+composer require gitzaai/mobile-ui-tab-2
+# 讨论书签，为讨论添加书签，以便日后轻松找到它们
+composer require clarkwinkelmann/flarum-ext-discussion-bookmarks
+# 阅读时间，向 Flarum 添加读取时间进度条
+composer require justoverclock/thread-read-time:"*"
+# 移动搜索，将移动搜索移至首页
+composer require flarumtr/flarum-ext-mobile-search
+# 蛋糕日，在加入周年纪念日，在成员姓名旁边显示生日蛋糕表情符号。
+composer require nearata/flarum-ext-cakeday:"*"
+# 作者更改，让模组更新讨论和帖子的作者和日期
+composer require clarkwinkelmann/flarum-ext-author-change
+# 色圈，根据用户组在头像周围添加一个颜色圆圈
+composer require clarkwinkelmann/flarum-ext-circle-groups
+# FoF Follow标签，关注标签并收到新讨论和回复的通知
+composer require fof/follow-tags:"*"
+# FoF 订阅，论坛的其他用户订阅
+composer require fof/subscribed:"*"
+#识别 Github Link
+composer require fof/github-autolink
+#个人主页留言板
+composer require fof/moderator-notes
+# Tex 数学公式支持
+composer require the-turk/flarum-mathren
+#标签颜色生成
+composer require nearata/flarum-ext-tags-color-generator
+# 在后台查看错误日志
+composer require ianm/log-viewer
+# 下雪
+composer require justoverclock/christmas-snow-effect
+# 嵌入Niconico/ACFUN/虾米音乐/网易云音乐/Bilibili
+composer require zequeen/acgembed-remastered
+#个人主页封面图片
+composer require sycho/flarum-profile-cover
+# SEO
+composer require v17development/flarum-seo
+
+
+# 未来更新
+# FoF页面，使用 Flarum 创建自定义页面
+# composer require fof/pages
+# FoF OAuth，允许用户使用 GitHub、Twitter、Facebook、Google 等登录！
+# composer require fof/oauth
+# FoF格式，通过管理界面使用插件自定义 TextFormatter
+# composer require fof/formatting:"*"
+# FoF 反垃圾邮件，管理社区中垃圾邮件发送者的有效工具。
+# composer require fof/anti-spam:"*"
+# FoF reCAPTCHA，使用 Google reCAPTCHA 提高论坛的安全性
+# composer require fof/recaptcha:"*"
+# 双因素,允许用户激活 2FA 身份验证。
+# composer require nearata/flarum-ext-twofactor:"*"
+# 预定义头像,让用户从列表中选择自己的头像
+# composer require clarkwinkelmann/flarum-ext-predefined-avatars:*
+# 论坛小部件，用于管理论坛小部件的核心扩展
+# https://extiverse.com/extension/afrux/forum-widgets-core
+# composer require afrux/forum-widgets-core:"*"
+# Mercury，用于了解扩展更新的 Extiverse 扩展
+# https://discuss.flarum.org/d/27620-mercury-the-extiverse-extension-to-understand-extension-updates
+# composer require extiverse/mercury:"*"
+
+
+
+# Mobile tab
+# https://extiverse.com/extension/acpl/mobile-tab
+
+
+# 热度
+composer require fof/gamification
+# 书签
+composer require clarkwinkelmann/flarum-ext-bookmarks
+# 图片放大
+composer require tohsakarat/fancybox-plus
+# 点击图片弹出查看窗口
+composer require darkle/fancybox
+#编辑器预览
+composer require zerosonesfun/composer-preview
+#生成一些虚假数据，例如：用户，帖子
+#composer require migratetoflarum/fake-data:*
+#论坛国际化翻译
+composer require fof/linguist
+#文件上传
+composer require fof/upload
+
+
+# 移除
+# 渐进式 Web 应用程序，将您的 flarum 网站变成一个渐进式 Web 应用程序！
+composer remove askvortsov/flarum-pwa
---
+++ 论坛/顶部js.js
@@ -0,0 +1,2 @@
+<link href="https://cdn.staticfile.org/lxgw-wenkai-screen-webfont/1.7.0/lxgwwenkaiscreen.css" rel="stylesheet"></link>
+<script async src="/assets/fireworks.js"></script>
