49855a4105e408217b4532884a8fac5e08d28e1f
draper-crypto <62155410+Draper-crypto@users.noreply.github.com>
2023-12-27 23:16:26 +0800

    change

--- 论坛/顶部js.js
+++ 论坛/顶部js.js
@@ -1,2 +1,2 @@
-<link href="https://cdn.staticfile.org/lxgw-wenkai-screen-webfont/1.7.0/lxgwwenkaiscreen.css" rel="stylesheet"></link>
 <script async src="/assets/fireworks.js"></script>
+<link href="https://cdn.staticfile.org/lxgw-wenkai-screen-webfont/1.7.0/lxgwwenkaiscreen.css" rel="stylesheet">
